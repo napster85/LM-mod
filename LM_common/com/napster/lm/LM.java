b61c15ae8b9add54bd937c25e07c1897b017306d
napster85 <napster-napster@hotmail.com>
2013-04-19 20:54:33 +0300

    test test test 3

---
+++ LM_common/com/napster/lm/LM.java
@@ -0,0 +1,29 @@
+package com.napster.lm;
+
+import com.napster.lm.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID,name = Reference.MOD_NAME, version = Reference.VERSION)
+public class LM {
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event){
+        
+    }
+    
+    @Init
+    public void Init(FMLInitializationEvent event){
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event){
+        
+    }
+}
---
+++ LM_common/com/napster/lm/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.napster.lm.lib;
+
+public class Reference {
+    public static final String MOD_ID="LM";
+    public static final String MOD_NAME="Let's Mod";
+    public static final String VERSION="0.0.1";
+
+}
